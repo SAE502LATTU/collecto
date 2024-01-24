9fedc8162002767cddb25ea117f8d0b2dcd345fd
CHEVALIERTho <128820172+CHEVALIERTho@users.noreply.github.com>
2024-01-24 11:21:11 +0000

    Ajout_page_audit

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -2,3 +2,6 @@ from flask import render_template
 from views import *
 def index():
     return render_template('index.html')
+
+def audit():
+    return render_template('audit.html')
--- src/routes/blueprint.py
+++ src/routes/blueprint.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
-from controllers.controller import index
+from controllers.controller import index, audit
 
 blueprint = Blueprint('blueprint', __name__)
 
 blueprint.route('/', methods=['GET'])(index)
+
+blueprint.route('/audit', methods=['GET'])(audit)
